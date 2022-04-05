b9576d0616092084df9783c3ba0834024da68f65
andrewfarmer13 <andrewfarmer13@gmail.com>
2022-04-04 23:45:46 -0500

    dfs,bfs,dijkstra, and updated main

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ Clayton McEntire, Andrew Farmer, Cameron Burdine
 """
 
 import networkx as nx ##This will be what we will use for the graph
+import dfs_path as dfs
+import bfs_paths as bfs
+import dijkstraAlgo as dij
 
 ###The following code will need to be deleted, its purpose is to figure out how
 ###networkx works
 
-G = nx.Graph() ##creates a new graph
-H = nx.Graph()
-H.add_node(10)
-G.add_node(2)
-G.add_node(1)##adds the node 1 to graph G
-H.add_nodes_from(G)##This should add all the nodes from G to H
-H.add_edge(10,1)
-H.add_edge(10,2)
-H.add_edge(1,2)
+# G = nx.Graph() ##creates a new graph
+# H = nx.Graph()
+# H.add_node(10)
+# G.add_node(2)
+# G.add_node(1)##adds the node 1 to graph G
+# H.add_nodes_from(G)##This should add all the nodes from G to H
+# H.add_edge(10,1)
+# H.add_edge(10,2)
+# H.add_edge(1,2)
 
-print(H)##prints the number of nodes and edges
+# ##print(H)##prints the number of nodes and edges
 
-print(list(H.nodes))##prints the nodes in a list
-print(list(H.edges))##prints the edges in a list of sets
+# print(list(H.nodes))##prints the nodes in a list
+# print(list(H.edges))##prints the edges in a list of sets
 
-H.remove_node(1)
+# H.remove_node(1)
 
-print(list(H.nodes))
-print(list(H.edges))
+# print(list(H.nodes))
+# print(list(H.edges))
 
 ###The Following code should set up the graph for question1
 A = nx.Graph()
@@ -68,5 +71,11 @@ A.add_edge("K", "L")
 A.add_edge("L", "P")
 
 
-print(list(A.nodes))
-print(list(A.edges))
+##print(list(A.nodes))
+##print(list(A.edges))
+
+v = dfs.dfs_path(A, "A", "J")
+print("DFS PATH: " + str(v))
+
+
+nx.draw(A, with_labels=True, font_weight='bold')
